8775055c5ed9e5bf85e815181a0785316ed0a280
Aless <alessiocuddretto@hotmail.com>
2026-06-10 19:43:26 +0200

    Primo rilascio funzionante del BnB

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite"; // <-- Importiamo Tailwind v4
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/bnb-react-tailwind-template/", // <-- AGGIUNGI QUESTA RIGA
+  plugins: [
+    react(),
+    tailwindcss(), // <-- Diciamo a Vite di attivarlo
+  ],
+  base: "/bnb-react-tailwind-template/",
+  build: {
+    outDir: "docs",
+  },
 });
